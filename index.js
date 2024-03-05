18c70ab2d4adcf28bb21b222c7e5d1514f0c18d9
tnknguyen_ <nguyenngockhoitran@gmail.com>
2024-03-05 16:48:56 +0700

    commit 12: updated day 1

--- index.js
+++ index.js
@@ -10,213 +10,213 @@ function getLocalStorage(address){
 const data = {
     approaching: [
         {
-            "title": "npm-free LiveReload(ish): Simple scripts for asset watching and auto-reload in the browser",
-            "date": "12-05-2024",
+            "title": "1 March 2024",
+            "date": "03-01-2024",
             content: [
                 {
-                    "url": "https://codeforces.com/problemset/problem/1182/A",
-                    "urlTextContent": "click q3e uy12gu 12g yg12793 129ieb1h2 bibme"
+                    "url": "https://oj.vnoi.info/problem/cowgirl",
+                    "urlTextContent": "Bitmask DP"
                 },
-                {
-                    "url": "https://codeforces.com/problemset/problem/1182/A",
-                    "urlTextContent": "click me"
-                },
-                {
-                    "url": "https://codeforces.com/problemset/problem/1182/A",
-                    "urlTextContent": "click me"
-                },
-                {
-                    "url": "https://codeforces.com/problemset/problem/1182/A",
-                    "urlTextContent": "click me"
-                }
+                // {
+                //     "url": "https://codeforces.com/problemset/problem/1182/A",
+                //     "urlTextContent": "click me"
+                // },
+                // {
+                //     "url": "https://codeforces.com/problemset/problem/1182/A",
+                //     "urlTextContent": "click me"
+                // },
+                // {
+                //     "url": "https://codeforces.com/problemset/problem/1182/A",
+                //     "urlTextContent": "click me"
+                // }
             ]
         },
-        {
-            "title": "A manifesto for small, static, web apps",
-            "date": "12-05-2024",
-            "urlTextContent": "I’m having a lot of fun building small, static web apps. And there are certain principles I’m trying to stick to as I do it. Let’s see what they are… What is a manifesto anyway? As I started writing this, I asked myself: “Is this actually a manifesto? What is a manifesto?” But the definition […]",
-            "url": "https://rosswintle.uk/?fbclid=IwAR1DXBCzHtbdcME6XQhpvoiHqvPTH7x0vxP_38gUPJCGdqAJxMAnLEaZx84"
-        },
-        {
-            "title": "ex3",
-            "date": "12-05-2024",
-            "urlTextContent": "Regular readers will know about my love of simple, static-hosted, web-based apps that are easy to build and maintain, and free to host. But they are held back by the lack of dynamic data. Sure, you can store data in local storage, but what if you want to share data between browsers? Is there a […]",
-            "url": "https://rosswintle.uk/?fbclid=IwAR1DXBCzHtbdcME6XQhpvoiHqvPTH7x0vxP_38gUPJCGdqAJxMAnLEaZx84"
-        },
-        {
-            "title": "Title 4",
-            "date": "01-02-2024",
-            "urlTextContent": "Content of Title 4",
-            "url": "https://example.com/4"
-        },
-        {
-            "title": "Title 5",
-            "date": "15-03-2024",
-            "urlTextContent": "Content of Title 5",
-            "url": "https://example.com/5"
-        },
-        {
-            "title": "Title 6",
-            "date": "23-06-2024",
-            "urlTextContent": "Content of Title 6",
-            "url": "https://example.com/6"
-        },
-        {
-            "title": "Title 7",
-            "date": "19-09-2024",
-            "urlTextContent": "Content of Title 7",
-            "url": "https://example.com/7"
-        },
-        {
-            "title": "Title 8",
-            "date": "28-11-2024",
-            "urlTextContent": "Content of Title 8",
-            "url": "https://example.com/8"
-        },
-        {
-            "title": "Title 9",
-            "date": "31-10-2024",
-            "urlTextContent": "Content of Title 9",
-            "url": "https://example.com/9"
-        },
-        {
-            "title": "Title 10",
-            "date": "25-02-2024",
-            "urlTextContent": "Content of Title 10",
-            "url": "https://example.com/10"
-        },
-        {
-            "title": "Title 11",
-            "date": "14-05-2024",
-            "urlTextContent": "Content of Title 11",
-            "url": "https://example.com/11"
-        },
-        {
-            "title": "Title 12",
-            "date": "06-08-2024",
-            "urlTextContent": "Content of Title 12",
-            "url": "https://example.com/12"
-        },
-        {
-            "title": "Title 13",
-            "date": "17-09-2024",
-            "urlTextContent": "Content of Title 13",
-            "url": "https://example.com/13"
-        },
-        {
-            "title": "Title 14",
-            "date": "09-11-2024",
-            "urlTextContent": "Content of Title 14",
-            "url": "https://example.com/14"
-        },
-        {
-            "title": "Title 15",
-            "date": "12-03-2024",
-            "urlTextContent": "Content of Title 15",
-            "url": "https://example.com/15"
-        },
-        {
-            "title": "Title 16",
-            "date": "27-06-2024",
-            "urlTextContent": "Content of Title 16",
-            "url": "https://example.com/16"
-        },
-        {
-            "title": "Title 17",
-            "date": "03-01-2024",
-            "urlTextContent": "Content of Title 17 - DP, Tree",
-            "url": "https://example.com/17"
-        },
-        {
-            "title": "Title 18",
-            "date": "30-04-2024",
-            "urlTextContent": "Content of Title 18",
-            "url": "https://example.com/18"
-        },
-        {
-            "title": "Title 19",
-            "date": "18-07-2024",
-            "urlTextContent": "Content of Title 19",
-            "url": "https://example.com/19"
-        },
-        {
-            "title": "Title 20",
-            "date": "22-10-2024",
-            "urlTextContent": "Content of Title 20",
-            "url": "https://example.com/20"
-        },
-        {
-            "title": "Title 21",
-            "date": "05-12-2024",
-            "urlTextContent": "Content of Title 21",
-            "url": "https://example.com/21"
-        },
-        {
-            "title": "Title 22",
-            "date": "10-02-2024",
-            "urlTextContent": "Content of Title 22",
-            "url": "https://example.com/22"
-        },
-        {
-            "title": "Title 23",
-            "date": "08-04-2024",
-            "urlTextContent": "Content of Title 23",
-            "url": "https://example.com/23"
-        },
-        {
-            "title": "Title 24",
-            "date": "29-07-2024",
-            "urlTextContent": "Content of Title 24",
-            "url": "https://example.com/24"
-        },
-        {
-            "title": "Title 25",
-            "date": "01-06-2024",
-            "urlTextContent": "Content of Title 25",
-            "url": "https://example.com/25"
-        },
-        {
-            "title": "Title 26",
-            "date": "26-08-2024",
-            "urlTextContent": "Content of Title 26",
-            "url": "https://example.com/26"
-        },
-        {
-            "title": "Title 27",
-            "date": "02-03-2024",
-            "urlTextContent": "Content of Title 27",
-            "url": "https://example.com/27"
-        },
-        {
-            "title": "Title 28",
-            "date": "20-05-2024",
-            "urlTextContent": "Content of Title 28",
-            "url": "https://example.com/28"
-        },
-        {
-            "title": "Title 29",
-            "date": "24-01-2024",
-            "urlTextContent": "Content of Title 29",
-            "url": "https://example.com/29"
-        },
-        {
-            "title": "Title 30",
-            "date": "16-10-2024",
-            "urlTextContent": "Content of Title 30",
-            "url": "https://example.com/30"
-        },
-        {
-            "title": "Title 31",
-            "date": "11-11-2024",
-            "urlTextContent": "Content of Title 31",
-            "url": "https://example.com/31"
-        },
-        {
-            "title": "Title 32",
-            "date": "04-01-2024",
-            "urlTextContent": "Content of Title 32",
-            "url": "https://example.com/32"
-        }
+        // {
+        //     "title": "A manifesto for small, static, web apps",
+        //     "date": "12-05-2024",
+        //     "urlTextContent": "I’m having a lot of fun building small, static web apps. And there are certain principles I’m trying to stick to as I do it. Let’s see what they are… What is a manifesto anyway? As I started writing this, I asked myself: “Is this actually a manifesto? What is a manifesto?” But the definition […]",
+        //     "url": "https://rosswintle.uk/?fbclid=IwAR1DXBCzHtbdcME6XQhpvoiHqvPTH7x0vxP_38gUPJCGdqAJxMAnLEaZx84"
+        // },
+        // {
+        //     "title": "ex3",
+        //     "date": "12-05-2024",
+        //     "urlTextContent": "Regular readers will know about my love of simple, static-hosted, web-based apps that are easy to build and maintain, and free to host. But they are held back by the lack of dynamic data. Sure, you can store data in local storage, but what if you want to share data between browsers? Is there a […]",
+        //     "url": "https://rosswintle.uk/?fbclid=IwAR1DXBCzHtbdcME6XQhpvoiHqvPTH7x0vxP_38gUPJCGdqAJxMAnLEaZx84"
+        // },
+        // {
+        //     "title": "Title 4",
+        //     "date": "01-02-2024",
+        //     "urlTextContent": "Content of Title 4",
+        //     "url": "https://example.com/4"
+        // },
+        // {
+        //     "title": "Title 5",
+        //     "date": "15-03-2024",
+        //     "urlTextContent": "Content of Title 5",
+        //     "url": "https://example.com/5"
+        // },
+        // {
+        //     "title": "Title 6",
+        //     "date": "23-06-2024",
+        //     "urlTextContent": "Content of Title 6",
+        //     "url": "https://example.com/6"
+        // },
+        // {
+        //     "title": "Title 7",
+        //     "date": "19-09-2024",
+        //     "urlTextContent": "Content of Title 7",
+        //     "url": "https://example.com/7"
+        // },
+        // {
+        //     "title": "Title 8",
+        //     "date": "28-11-2024",
+        //     "urlTextContent": "Content of Title 8",
+        //     "url": "https://example.com/8"
+        // },
+        // {
+        //     "title": "Title 9",
+        //     "date": "31-10-2024",
+        //     "urlTextContent": "Content of Title 9",
+        //     "url": "https://example.com/9"
+        // },
+        // {
+        //     "title": "Title 10",
+        //     "date": "25-02-2024",
+        //     "urlTextContent": "Content of Title 10",
+        //     "url": "https://example.com/10"
+        // },
+        // {
+        //     "title": "Title 11",
+        //     "date": "14-05-2024",
+        //     "urlTextContent": "Content of Title 11",
+        //     "url": "https://example.com/11"
+        // },
+        // {
+        //     "title": "Title 12",
+        //     "date": "06-08-2024",
+        //     "urlTextContent": "Content of Title 12",
+        //     "url": "https://example.com/12"
+        // },
+        // {
+        //     "title": "Title 13",
+        //     "date": "17-09-2024",
+        //     "urlTextContent": "Content of Title 13",
+        //     "url": "https://example.com/13"
+        // },
+        // {
+        //     "title": "Title 14",
+        //     "date": "09-11-2024",
+        //     "urlTextContent": "Content of Title 14",
+        //     "url": "https://example.com/14"
+        // },
+        // {
+        //     "title": "Title 15",
+        //     "date": "12-03-2024",
+        //     "urlTextContent": "Content of Title 15",
+        //     "url": "https://example.com/15"
+        // },
+        // {
+        //     "title": "Title 16",
+        //     "date": "27-06-2024",
+        //     "urlTextContent": "Content of Title 16",
+        //     "url": "https://example.com/16"
+        // },
+        // {
+        //     "title": "Title 17",
+        //     "date": "03-01-2024",
+        //     "urlTextContent": "Content of Title 17 - DP, Tree",
+        //     "url": "https://example.com/17"
+        // },
+        // {
+        //     "title": "Title 18",
+        //     "date": "30-04-2024",
+        //     "urlTextContent": "Content of Title 18",
+        //     "url": "https://example.com/18"
+        // },
+        // {
+        //     "title": "Title 19",
+        //     "date": "18-07-2024",
+        //     "urlTextContent": "Content of Title 19",
+        //     "url": "https://example.com/19"
+        // },
+        // {
+        //     "title": "Title 20",
+        //     "date": "22-10-2024",
+        //     "urlTextContent": "Content of Title 20",
+        //     "url": "https://example.com/20"
+        // },
+        // {
+        //     "title": "Title 21",
+        //     "date": "05-12-2024",
+        //     "urlTextContent": "Content of Title 21",
+        //     "url": "https://example.com/21"
+        // },
+        // {
+        //     "title": "Title 22",
+        //     "date": "10-02-2024",
+        //     "urlTextContent": "Content of Title 22",
+        //     "url": "https://example.com/22"
+        // },
+        // {
+        //     "title": "Title 23",
+        //     "date": "08-04-2024",
+        //     "urlTextContent": "Content of Title 23",
+        //     "url": "https://example.com/23"
+        // },
+        // {
+        //     "title": "Title 24",
+        //     "date": "29-07-2024",
+        //     "urlTextContent": "Content of Title 24",
+        //     "url": "https://example.com/24"
+        // },
+        // {
+        //     "title": "Title 25",
+        //     "date": "01-06-2024",
+        //     "urlTextContent": "Content of Title 25",
+        //     "url": "https://example.com/25"
+        // },
+        // {
+        //     "title": "Title 26",
+        //     "date": "26-08-2024",
+        //     "urlTextContent": "Content of Title 26",
+        //     "url": "https://example.com/26"
+        // },
+        // {
+        //     "title": "Title 27",
+        //     "date": "02-03-2024",
+        //     "urlTextContent": "Content of Title 27",
+        //     "url": "https://example.com/27"
+        // },
+        // {
+        //     "title": "Title 28",
+        //     "date": "20-05-2024",
+        //     "urlTextContent": "Content of Title 28",
+        //     "url": "https://example.com/28"
+        // },
+        // {
+        //     "title": "Title 29",
+        //     "date": "24-01-2024",
+        //     "urlTextContent": "Content of Title 29",
+        //     "url": "https://example.com/29"
+        // },
+        // {
+        //     "title": "Title 30",
+        //     "date": "16-10-2024",
+        //     "urlTextContent": "Content of Title 30",
+        //     "url": "https://example.com/30"
+        // },
+        // {
+        //     "title": "Title 31",
+        //     "date": "11-11-2024",
+        //     "urlTextContent": "Content of Title 31",
+        //     "url": "https://example.com/31"
+        // },
+        // {
+        //     "title": "Title 32",
+        //     "date": "04-01-2024",
+        //     "urlTextContent": "Content of Title 32",
+        //     "url": "https://example.com/32"
+        // }
 
     ]
 }
